daec93a20115099e2740fada6f1d20d7d78213e9
Mateus <st_mts@outlook.com>
2024-06-11 14:54:22 -0300

    Update ContaTerminal.java

--- conta-banco/src/ContaTerminal.java
+++ conta-banco/src/ContaTerminal.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 public class ContaTerminal {
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) {
 
         // Conhecer e importar a classe Scanner
 
